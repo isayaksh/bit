e46504013934d45d287937b3d28ed922094df727
isayaksh <isayaksh@gmail.com>
2025-12-02 18:42:06 +0900

    init project

---
+++ src/main/kotlin/com/monitor/bit/BitApplication.kt
@@ -0,0 +1,11 @@
+package com.monitor.bit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BitApplication
+
+fun main(args: Array<String>) {
+	runApplication<BitApplication>(*args)
+}
---
+++ src/test/kotlin/com/monitor/bit/BitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.monitor.bit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
