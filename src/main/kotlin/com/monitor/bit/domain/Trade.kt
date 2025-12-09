037bfea33249b09b63b8d9cbf72be4f6f84b30ce
isayaksh <isayaksh@gmail.com>
2025-12-09 18:52:06 +0900

    create trade entity

---
+++ src/main/kotlin/com/monitor/bit/domain/Trade.kt
@@ -0,0 +1,48 @@
+package com.monitor.bit.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.GenerationType.*
+import jakarta.persistence.Table
+import org.springframework.data.annotation.Id
+
+@Entity
+@Table(name = "trade")
+class Trade(
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    val id: Long? = null,
+
+    @Column(nullable = false, length = 20)
+    val market: String,
+
+    @Column(nullable = false, length = 10)
+    val tradeDateUtc: String,
+
+    @Column(nullable = false, length = 10)
+    val tradeTimeUtc: String,
+
+    @Column(nullable = false)
+    val timestamp: Long,
+
+    @Column(nullable = false)
+    val tradePrice: Double,
+
+    @Column(nullable = false)
+    val tradeVolume: Double,
+
+    @Column(nullable = false)
+    val prevClosingPrice: Double,
+
+    @Column(nullable = false)
+    val changePrice: Double,
+
+    @Column(nullable = false, length = 3)
+    val askBid: String,
+
+    @Column(nullable = false)
+    val sequentialId: Long
+)
