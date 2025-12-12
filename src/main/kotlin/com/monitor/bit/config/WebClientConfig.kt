5df7cb1270019a23709dd1b0f71ac869f5919558
isayaksh <isayaksh@gmail.com>
2025-12-12 20:45:34 +0900

    separate environment value

--- src/main/kotlin/com/monitor/bit/config/WebClientConfig.kt
+++ src/main/kotlin/com/monitor/bit/config/WebClientConfig.kt
@@ -1,16 +1,20 @@
 package com.monitor.bit.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
 @Component
-class WebClientConfig {
+class WebClientConfig(
+    @Value("\${upbit.api.base-url}")
+    private val BASE_URL: String
+) {
 
     @Bean
     fun webClient(): WebClient {
         return WebClient.builder()
-            .baseUrl("https://api.upbit.com")
+            .baseUrl(BASE_URL)
             .build()
     }
 
