600810ecd783a1d5468c1158ff20ba11f800298a
isayaksh <isayaksh@gmail.com>
2025-12-17 21:26:45 +0900

    create ticker writer

---
+++ src/main/kotlin/com/monitor/bit/writer/TickerWriter.kt
@@ -0,0 +1,20 @@
+package com.monitor.bit.writer
+
+import com.monitor.bit.domain.Ticker
+import com.monitor.bit.repository.TickerRepository
+import org.springframework.batch.infrastructure.item.Chunk
+import org.springframework.batch.infrastructure.item.ItemWriter
+import org.springframework.stereotype.Component
+
+@Component
+class TickerWriter(
+    private val tickerRepository: TickerRepository
+) : ItemWriter<List<Ticker>> {
+
+    override fun write(items: Chunk<out List<Ticker>>) {
+        items.flatten().let {
+            tickerRepository.saveAll(it)
+        }
+    }
+
+}
