58b5e9eb755a791e6b52b6fc650d4e0a66e41e59
isayaksh <isayaksh@gmail.com>
2025-12-18 21:08:44 +0900

    modify ticker item reader

--- src/main/kotlin/com/monitor/bit/reader/TickerItemReader.kt
+++ src/main/kotlin/com/monitor/bit/reader/TickerItemReader.kt
@@ -6,20 +6,15 @@ import org.springframework.stereotype.Component
 
 @Component
 class TickerItemReader(
-    private val tradingPairRepository: TradingPairRepository
-): ItemReader<List<String>> {
+    tradingPairRepository: TradingPairRepository
+) : ItemReader<List<String>> {
 
-    private var index = 0
-    private val markets: List<String> by lazy {
-        tradingPairRepository.findAll().map { it.market }
-    }
+    private val iterator: Iterator<List<String>> =
+        tradingPairRepository.findAll()
+            .map { it.market }
+            .chunked(100)
+            .iterator()
 
-    override fun read(): List<String>? {
-        if (index >= markets.size) return null
-
-        val chunk = markets.drop(index).take(100)
-        index += 100
-        return chunk
-    }
-
+    override fun read(): List<String>? =
+        if (iterator.hasNext()) iterator.next() else null
 }
