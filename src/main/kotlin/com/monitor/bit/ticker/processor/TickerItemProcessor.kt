859643ab5d5abb5929665696271d465660ab6e00
isayaksh <isayaksh@gmail.com>
2025-12-20 22:59:39 +0900

    create ticker processor

---
+++ src/main/kotlin/com/monitor/bit/ticker/processor/TickerItemProcessor.kt
@@ -0,0 +1,30 @@
+package com.monitor.bit.ticker.processor
+
+import com.monitor.bit.domain.Ticker
+import com.monitor.bit.dto.TickerDTO
+import org.springframework.batch.infrastructure.item.ItemProcessor
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.WebClient
+
+@Component
+class TickerItemProcessor(
+    private val webClient: WebClient
+) : ItemProcessor<List<String>, List<Ticker>> {
+
+    override fun process(markets: List<String>): List<Ticker> {
+        val marketParam = markets.joinToString(",")
+
+        return webClient.get()
+            .uri { uriBuilder ->
+                uriBuilder
+                    .path("/v1/ticker")
+                    .queryParam("markets", marketParam)
+                    .build()
+            }
+            .retrieve()
+            .bodyToFlux(TickerDTO::class.java)
+            .map { it.toEntity() }
+            .collectList()
+            .block() ?: emptyList()
+    }
+}
